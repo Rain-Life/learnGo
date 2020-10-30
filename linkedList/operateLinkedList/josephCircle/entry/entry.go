a29c69dd71d0209d5cc01fca55669e9044d51abf
陈兴生 <chenxingsheng@51shebao.com>
2020-10-30 20:41:53 +0800

    单向循环链表解决约瑟夫问题

--- linkedList/operateLinkedList/josephCircle/entry/entry.go
+++ linkedList/operateLinkedList/josephCircle/entry/entry.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"learnGo/linkedList/operateLinkedList/josephCircle"
+)
+
 func main() {
-	
+	list := josephCircle.List{}
+	data := []josephCircle.Object{1,2,3,4,5,6,7,8}
+	list.CreateLoopList(data)
+	list.Traverse()
+
+	fmt.Println("单向循环链表解决约瑟夫问题")
+	list.
 }
--- linkedList/operateLinkedList/josephCircle/josephCircle.go
+++ linkedList/operateLinkedList/josephCircle/josephCircle.go
@@ -1,5 +1,80 @@
-package main
+package josephCircle
 
-func main() {
-	
+import "fmt"
+
+type Object interface {}
+
+type Node struct {
+	Data Object
+	Next *Node
+}
+
+type List struct {
+	headNode *Node
+}
+
+//遍历链表
+func (list List)Traverse()  {
+	if list.headNode == nil {
+		fmt.Printf("链表为空")
+		return
+	}
+
+	currentNode := list.headNode
+	for currentNode.Next != list.headNode {
+		fmt.Printf("%v\t", currentNode.Data)
+		currentNode = currentNode.Next
+	}
+	fmt.Printf("%v\t", currentNode.Data)
+}
+
+//生成循环链表
+func (list *List)CreateLoopList(data []Object)  {
+	if len(data) < 0 {
+		fmt.Println("参数不合法")
+		return
+	}
+
+	currentNode := list.headNode
+	for i, value := range data {
+		node := &Node{Data: value}
+		if i == 0 {
+			list.headNode = node
+			currentNode = node
+			continue
+		}
+		currentNode.Next = node
+		currentNode = node
+	}
+	currentNode.Next = list.headNode
+}
+
+//单向链表解决约瑟夫问题
+func (list *List) dealJosephCircle(number int) []interface{} {
+	//需要考虑链表只有一个节点的情况
+	var data []interface{}
+	index := 1
+	currentNode := list.headNode
+	//preNode := list.headNode
+
+	for list.headNode != nil {
+		//只有一个节点的情况
+		if currentNode.Next == list.headNode {
+			data = append(data, currentNode.Data)
+			list.headNode = nil
+			continue
+		}
+
+		if index == number {
+			//删除结点，其实不用考虑是不是头结点或尾结点
+
+
+			index = 0
+		} else {
+			index++
+		}
+
+		currentNode = currentNode.Next
+	}
+	return data
 }
